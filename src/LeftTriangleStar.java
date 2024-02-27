782d5bab65085773318cb7bfad1e0f3e92be17ae
basakozek <basakozek3@gmail.com>
2024-02-27 19:11:18 +0300

    Başak Özek

---
+++ src/LeftTriangleStar.java
@@ -0,0 +1,20 @@
+public class LeftTriangleStar
+{
+  public static void main(String args[])
+  {
+    int row, column, numberOfRows = 5;
+    for (row=0; row<numberOfRows; row++)
+    {
+      for (column=2*(numberOfRows-row); column>2; column--)
+      {
+        System.out.print(" ");
+      }
+      for (column=0; column<=row; column++ )
+      {
+        System.out.print("* ");
+      }
+      //To move cursor to new line for each row
+      System.out.println();
+    }
+  }
+}
